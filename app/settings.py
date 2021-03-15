da57c5982d6e98761b22b98fa5a715a14730c309
FaridLesther <61443299+FaridLesther@users.noreply.github.com>
2021-03-15 00:57:01 -0600

    ya me canse

--- app/settings.py
+++ app/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('HEROKU_POSTGRESQL_CHARCOAL_URL'))
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
