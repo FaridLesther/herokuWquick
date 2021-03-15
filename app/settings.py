82d61cc26030da87051bf392f06aa143023fb4ea
FaridLesther <61443299+FaridLesther@users.noreply.github.com>
2021-03-15 01:09:30 -0600

    q

--- app/settings.py
+++ app/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('HEROKU_POSTGRESQL_AQUA_URL'))
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
