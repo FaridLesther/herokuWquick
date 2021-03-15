9fcc208a944a777e551292dd32fa706fc4c51885
FaridLesther <61443299+FaridLesther@users.noreply.github.com>
2021-03-15 01:14:47 -0600

    ya

--- app/settings.py
+++ app/settings.py
@@ -128,7 +128,6 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
